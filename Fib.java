4174ca8c5fbe00775abcae10b4a684dce884f769
Stephon Tidd <stidd@mail.valenciacollege.edu>
2023-09-05 21:00:43 -0400

    first commit for branch_1

--- Fib.java
+++ Fib.java
@@ -10,6 +10,15 @@ public class Fib {
         }
     }
 
+    /**
+     * This function is recursive and take a param.
+     * @param n is the int passed
+     * subtracts n by 1 and 2.
+     * if(base is True) return n
+     *
+     * @return fibonacci(n - 1) + fibonacci(n - 2)
+     * input# 10: fibonacci(10 - 1) + fibonacci(10 - 2)= fib(9) + fib(8)............
+     */
     public static int fibonacci(int n){
         if (n <= 1)
             return n;
